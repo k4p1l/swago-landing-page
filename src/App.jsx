4d3cbd2d4d70cfd3863ada64f93bc63efe7fc486
kapil <k4p1ll.23@gmail.com>
2025-01-18 17:22:03 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
+import { Footer } from "./components/Footer";
 
 const LandingPage = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,6 +10,7 @@ const LandingPage = () => {
     <div>
       <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
       <Hero />
+      <Footer />
     </div>
   );
 };
