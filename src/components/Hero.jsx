8a2b9b65e89d4d8ba6ce5dd82b416910ed976f6e
kapil <k4p1ll.23@gmail.com>
2025-01-25 18:38:51 +0530

    added cards

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import mascot from "../assets/images/Gogo and Nono.png";
+import card1 from "../assets/images/1.png";
+import card2 from "../assets/images/2.png";
+import card3 from "../assets/images/3.png";
 
 export const Hero = () => {
   return (
@@ -20,7 +23,20 @@ export const Hero = () => {
             </button>
           </div>
           <div className="flex items-center w-full">
-            <div className="sm:h-[400px] w-full h-[200px] gradient rounded-3xl"></div>
+            <div className="sm:h-[400px] w-full h-[200px] gradient rounded-3xl relative">
+              {/* Third card (bottom) */}
+              <div className="absolute left-1/2 top-1/2 sm:-translate-x-[0%] -translate-x-[10%] -translate-y-1/2 sm:w-[280px] w-[155px]">
+                <img className="w-full rounded-2xl" src={card3} alt="" />
+              </div>
+              {/* Second card (middle) */}
+              <div className="absolute left-1/2 top-1/2 sm:-translate-x-[40%] -translate-x-[50%] -translate-y-1/2 sm:w-[300px] w-[160px]">
+                <img className="w-full rounded-2xl" src={card2} alt="" />
+              </div>
+              {/* First card (top) */}
+              <div className="absolute left-1/2 top-1/2 sm:-translate-x-[80%] -translate-x-[90%] -translate-y-1/2 w-[170px] sm:w-[320px]">
+                <img className="w-full rounded-2xl" src={card1} alt="" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
